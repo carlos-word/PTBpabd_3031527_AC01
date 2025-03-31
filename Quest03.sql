aa220409ac99b6f8b5be381a6ba56fd936b945f3
Carlos Martins <carlosouza.ces@gmail.com>
2025-03-31 18:29:06 -0300

    Delete Quest03.sql

--- Quest03.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW civil_eng_students AS
-SELECT s.name AS aluno, COUNT(t.course_id) AS quantidade_cursos
-FROM student s
-JOIN takes t ON s.ID = t.ID
-WHERE s.dept_name = 'Civil Eng.'
-GROUP BY s.name;
-
-
-
